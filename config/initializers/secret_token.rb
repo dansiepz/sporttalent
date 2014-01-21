09795fdf94507590010060a6daa0c7b62e6bdde4
Dan Siepen <dan@thecoderfactory.com>
2014-01-21 17:38:41 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sporttalent::Application.config.secret_key_base = '2f58c35a84779e0cfc5679abd93c4573e3a459279bc4044b3335bdab61e8bbbac421f9ae936d9d042f9bbdc8fcee7c3a3967344490377fd461bf4ef3f35b5d2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sporttalent::Application.config.session_store :cookie_store, key: '_sporttalent_session'
